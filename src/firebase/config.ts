4426f6d7ad23cd59b84baae386889ec156e3e6ca
Piyush Bagde <piyushbagde.sistec@gmail.com>
2025-10-04 07:50:51 +0000

    Try fixing this error: `Console Error: [2025-10-04T07:45:11.326Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3285836963-946d4",
-  "appId": "1:620660611983:web:d88e98beff31d661a649fa",
-  "apiKey": "AIzaSyCxxA6fkOcMavBXo9nOAZ_ILZo35HeyHeI",
-  "authDomain": "studio-3285836963-946d4.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "620660611983"
+  projectId: 'agriclear-123',
+  appId: '1:620660611983:web:d88e98beff31d661a649fa',
+  storageBucket: 'agriclear-123.appspot.com',
+  apiKey: 'AIzaSyCxxA6fkOcMavBXo9nOAZ_ILZo35HeyHeI',
+  authDomain: 'agriclear-123.firebaseapp.com',
+  messagingSenderId: '620660611983',
 };
